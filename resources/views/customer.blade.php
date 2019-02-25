9529ec1d43378024089d0d662d3ca56cbeeb9dbb
Edita <edita.bojadzi@le-campus-numerique.fr>
2019-02-25 14:59:52 +0100

    wip - problem form address

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -19,10 +19,10 @@
                 <p>CIVILITE</p>
                 <form role="form">
                     <label class="radio-inline pl-3">
-                        <input type="radio" name="optradio">Madame
+                        <input type="radio" name="optradio" disabled>Madame
                     </label>
                     <label class="radio-inline pl-3">
-                        <input type="radio" name="optradio">Monsieur
+                        <input type="radio" name="optradio" disabled>Monsieur
                     </label>
                 </form>
             </div>
@@ -111,24 +111,25 @@
         <!-- FIN FORM MES INFORMATIONS-->
 
         <!-- DEBUT FORM MES ADRESSES-->
-        <form>
+
             <h2 class="mt-5">
                 <small>
                     MES ADRESSES
                 </small>
             </h2>
+        <div class="row">
+        <form class="col-sm-6 border border-dark">
 
-            <div class="container">
                 <div class="row mt-5">
-                    <div class="col-sm-6 border border-dark">
+
                         <h4>FACTURATION</h4>
                         <div class="form-row">
                             <div class="col-xs-4">
-                                <label for="inputFirstName20"></label>
+                                <label for="inputFirstName20">PRENOM</label>
                                 <input type="text" class="form-control" placeholder="Prénom" disabled>
                             </div>
                             <div class="col-xs-4">
-                                <label for="inputLastName"></label>
+                                <label for="inputLastName">NOM</label>
                                 <input type="text" class="form-control" placeholder="Nom" disabled>
                             </div>
                             <div class="form-row pt-4">
@@ -164,16 +165,87 @@
                                 </div>
                             </div>
                             <div class="col-xs-4">
-                                <label for="inputFirstName20"></label>
-                                <input type="int" class="form-control" placeholder="Numéro" disabled>
+                                <label for="inputFirstName20">NUMERO DE TELEPHONE</label>
+                                <input type="number" class="form-control" placeholder="Numéro" disabled>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="justify-content-end d-flex">
+                                <button type="submit" class="btn btn-primary d-block ">MODIFIER</button>
+                            </div>
+                            <div class="justify-content-end d-flex">
+                                <button type="submit" class="btn btn-primary mt-2">SUPPRIMER</button>
+                            </div>
+                        </div>
+
+                </div>
+        </form>
+
+        <form>
+            <div class="col-sm-6 other border border-dark">
+                <h4>LIVRAISON</h4>
+
+
+
+                        <div class="form-row">
+                            <div class="col-xs-4">
+                                <label for="inputFirstName20">PRENOM</label>
+                                <input type="text" class="form-control" placeholder="Prénom" disabled>
+                            </div>
+                            <div class="col-xs-4">
+                                <label for="inputLastName">NOM</label>
+                                <input type="text" class="form-control" placeholder="Nom" disabled>
+                            </div>
+                            <div class="form-row pt-4">
+                                <div class="col-xs-4">
+                                    <label for="inputEmail4">EMAIL</label>
+                                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email"
+                                           disabled>
+                                </div>
+
+                            </div>
+                            <div class="form-group pt-4 ml-3">
+                                <label for="inputAddress">ADRESSE</label>
+                                <input type="text" class="form-control" id="inputAddress"
+                                       placeholder="10 rue des petites chattes"
+                                       disabled>
+                            </div>
+
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="inputCity">VILLE</label>
+                                    <input type="text" class="form-control" id="inputCity" placeholder="Paris"
+                                           disabled>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="inputState">PAYS</label>
+                                    <select id="inputState" class="form-control" disabled>
+                                        <option selected>SELECTIONNER</option>
+                                        <option>France</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="inputZip">CODE POSTAL</label>
+                                    <input type="text" class="form-control" id="inputZip" disabled>
+                                </div>
+                            </div>
+                            <div class="col-xs-4">
+                                <label for="inputFirstName20">NUMERO DE TELEPHONE</label>
+                                <input type="number" class="form-control" placeholder="Numéro" disabled>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="justify-content-end d-flex">
+                                <button type="submit" class="btn btn-primary d-block ">AJOUTER</button>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-6 other border border-dark">
-                        <h4>LIVRAISON</h4>
-                    </div>
+
+                <div class="col-sm-6 other border border-dark">
                 </div>
-            </div>
+
         </form>
-            <!-- FIN FORM MES ADRESSES-->
+        </div>
+        <!-- FIN FORM MES ADRESSES-->
+    </div>
 @stop
