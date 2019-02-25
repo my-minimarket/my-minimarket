559fa0a1eb5635106eb9ab298d3c7a2c400768c4
Edita <edita.bojadzi@le-campus-numerique.fr>
2019-02-25 13:14:28 +0100

    wip - update customer page

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -1,57 +1,120 @@
 @extends('layouts.page')
 
 @section('content')
+    <h2 class="text-center"><small>
+        MES INFORMATIONS
+        </small></h2>
     <div class="ficheclient">
         <form>
+            <div class="container p-0 d-flex">
+                <p>CIVILITE</p>
+                <form role="form">
+                    <label class="radio-inline pl-3">
+                        <input type="radio" name="optradio">Madame
+                    </label>
+                    <label class="radio-inline pl-3">
+                        <input type="radio" name="optradio">Monsieur
+                    </label>
+                </form>
+            </div>
         <div class="form-row">
-            <div class="col">
+            <div class="col-xs-4">
                 <label for="inputFirstName20">PRENOM</label>
-                <input type="text" class="form-control" placeholder="Prénom">
-            <!--</div>
-            <div class="col">-->
+                <input type="text" class="form-control" placeholder="Prénom" disabled>
+            </div>
+            <div class="col-xs-4">
                 <label for="inputLastName">NOM</label>
-                <input type="text" class="form-control" placeholder="Nom">
+                <input type="text" class="form-control" placeholder="Nom" disabled>
             </div>
         </div>
 
             <div class="form-row pt-4" >
-                <div class="form-group col-md-6">
+                <div class="col-xs-4">
                     <label for="inputEmail4">EMAIL</label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email" disabled>
                 </div>
-                <div class="form-group col-md-6">
-                    <label for="inputPassword4">MOT DE PASSE</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="Mot de passe">
-                </div>
+
             </div>
             <div class="form-group">
                 <label for="inputAddress">ADRESSE</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="10 rue des petites chattes">
+                <input type="text" class="form-control" id="inputAddress" placeholder="10 rue des petites chattes" disabled>
             </div>
-            <div class="form-group">
-                <label for="inputAddress2">COMPLEMENT</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Etage, autres">
-            </div>
+
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="inputCity">VILLE</label>
-                    <input type="text" class="form-control" id="inputCity" placeholder="Paris">
+                    <input type="text" class="form-control" id="inputCity" placeholder="Paris" disabled>
                 </div>
                 <div class="form-group col-md-4">
                     <label for="inputState">PAYS</label>
-                    <select id="inputState"  class="form-control">
+                    <select id="inputState"  class="form-control" disabled>
                         <option selected>SELECTIONNER</option>
                         <option>France</option>
                     </select>
                 </div>
                 <div class="form-group col-md-2">
                     <label for="inputZip">CODE POSTAL</label>
-                    <input type="text" class="form-control" id="inputZip">
+                    <input type="text" class="form-control" id="inputZip" disabled>
                 </div>
             </div>
             <div class="form-group">
             </div>
-            <button type="submit" class="btn btn-primary">Sign in</button>
+
+            <div class="form-group">
+                <label for="sel1">DATE DE NAISSANCE</label>
+                <select class="col-xs-2 pl-3" id="sel1" disabled>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+
+                <select class="col-xs-3 pl-3" id="sel1" disabled>
+                    <option>Janvier</option>
+                    <option>Fevrier</option>
+                    <option>Mars</option>
+                    <option>Avril</option>
+                    <option>Mai</option>
+                    <option>Juin</option>
+                    <option>Juillet</option>
+                    <option>Aout</option>
+                    <option>Septembre</option>
+                    <option>Octobre</option>
+                    <option>Octobre</option>
+                    <option>Decembre</option>
+                </select>
+
+                <select class="col-xs-3" id="sel1" disabled>
+                    <option>1950</option>
+                </select>
+            </div>
+
+            <button type="submit" class="btn btn-primary d-block">CHANGER DE MDP</button>
+            <button type="submit" class="btn btn-primary mt-2">MODIFIER MES INFORMATIONS</button>
+        </form>
+
+        <form>
+
+            <h2 class="text-center"><small>
+                    MES ADRESSES
+                </small></h2>
+            <div class="container mt-5">
+                <div class="row col-lg-4">
+                    <div class="col-sm-8">
+                    <h3 class="text-center"><small>FACTURATION</small></h3>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+        <form>
+            <div class="container mt-5">
+                <div class="row col-lg-30">
+                    <div class="col-sm-8">
+                        <h3 class="text-center"><small>LIVRAISON</small></h3>
+                    </div>
+                </div>
+            </div>
         </form>
 
     </div>
