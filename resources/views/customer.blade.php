d97e49612e9b264bb9b2ac2e2de644bef6448913
Edita <edita.bojadzi@le-campus-numerique.fr>
2019-02-25 14:25:17 +0100

    wip - facturation

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -2,10 +2,18 @@
 
 @section('content')
     <link rel="stylesheet" type="text/css" href="{{ asset('css/customer.css') }}">
-    <h2 class="text-center"><small>
-        MES INFORMATIONS
-        </small></h2>
-    <div class="ficheclient">
+
+    <div class="text-center">
+        <h1>MON COMPTE</h1>
+    </div>
+
+    <!-- DEBUT FORM MES INFORMATIONS-->
+    <h2>
+        <small>
+            MES INFORMATIONS
+        </small>
+    </h2>
+    <div class="ficheclient mt-5">
         <form>
             <div class="container p-0 d-flex">
                 <p>CIVILITE</p>
@@ -18,18 +26,18 @@
                     </label>
                 </form>
             </div>
-        <div class="form-row">
-            <div class="col-xs-4">
-                <label for="inputFirstName20">PRENOM</label>
-                <input type="text" class="form-control" placeholder="Prénom" disabled>
+            <div class="form-row">
+                <div class="col-xs-4">
+                    <label for="inputFirstName20">PRENOM</label>
+                    <input type="text" class="form-control" placeholder="Prénom" disabled>
+                </div>
+                <div class="col-xs-4">
+                    <label for="inputLastName">NOM</label>
+                    <input type="text" class="form-control" placeholder="Nom" disabled>
+                </div>
             </div>
-            <div class="col-xs-4">
-                <label for="inputLastName">NOM</label>
-                <input type="text" class="form-control" placeholder="Nom" disabled>
-            </div>
-        </div>
 
-            <div class="form-row pt-4" >
+            <div class="form-row pt-4">
                 <div class="col-xs-4">
                     <label for="inputEmail4">EMAIL</label>
                     <input type="email" class="form-control" id="inputEmail4" placeholder="Email" disabled>
@@ -38,7 +46,8 @@
             </div>
             <div class="form-group">
                 <label for="inputAddress">ADRESSE</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="10 rue des petites chattes" disabled>
+                <input type="text" class="form-control" id="inputAddress" placeholder="10 rue des petites chattes"
+                       disabled>
             </div>
 
             <div class="form-row">
@@ -48,7 +57,7 @@
                 </div>
                 <div class="form-group col-md-4">
                     <label for="inputState">PAYS</label>
-                    <select id="inputState"  class="form-control" disabled>
+                    <select id="inputState" class="form-control" disabled>
                         <option selected>SELECTIONNER</option>
                         <option>France</option>
                     </select>
@@ -89,34 +98,82 @@
                     <option>1950</option>
                 </select>
             </div>
+            <div>
+                <div class="justify-content-end d-flex">
+                    <button type="submit" class="btn btn-primary d-block ">CHANGER DE MDP</button>
+                </div>
+                <div class="justify-content-end d-flex">
+                    <button type="submit" class="btn btn-primary mt-2">MODIFIER MES INFORMATIONS</button>
+                </div>
 
-            <button type="submit" class="btn btn-primary d-block">CHANGER DE MDP</button>
-            <button type="submit" class="btn btn-primary mt-2">MODIFIER MES INFORMATIONS</button>
+            </div>
         </form>
+        <!-- FIN FORM MES INFORMATIONS-->
 
+        <!-- DEBUT FORM MES ADRESSES-->
         <form>
-
-            <h2 class="text-center"><small>
+            <h2 class="mt-5">
+                <small>
                     MES ADRESSES
-                </small></h2>
-            <div class="container mt-5">
-                <div class="row col-lg-4">
-                    <div class="col-sm-8">
-                    <h3 class="text-center"><small>FACTURATION</small></h3>
+                </small>
+            </h2>
+
+            <div class="container">
+                <div class="row mt-5">
+                    <div class="col-sm-6 border border-dark">
+                        <h4>FACTURATION</h4>
+                        <div class="form-row">
+                            <div class="col-xs-4">
+                                <label for="inputFirstName20"></label>
+                                <input type="text" class="form-control" placeholder="Prénom" disabled>
+                            </div>
+                            <div class="col-xs-4">
+                                <label for="inputLastName"></label>
+                                <input type="text" class="form-control" placeholder="Nom" disabled>
+                            </div>
+                            <div class="form-row pt-4">
+                                <div class="col-xs-4">
+                                    <label for="inputEmail4">EMAIL</label>
+                                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email"
+                                           disabled>
+                                </div>
+
+                            </div>
+                            <div class="form-group pt-4 ml-3">
+                                <label for="inputAddress">ADRESSE</label>
+                                <input type="text" class="form-control" id="inputAddress"
+                                       placeholder="10 rue des petites chattes"
+                                       disabled>
+                            </div>
+
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="inputCity">VILLE</label>
+                                    <input type="text" class="form-control" id="inputCity" placeholder="Paris" disabled>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="inputState">PAYS</label>
+                                    <select id="inputState" class="form-control" disabled>
+                                        <option selected>SELECTIONNER</option>
+                                        <option>France</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="inputZip">CODE POSTAL</label>
+                                    <input type="text" class="form-control" id="inputZip" disabled>
+                                </div>
+                            </div>
+                            <div class="col-xs-4">
+                                <label for="inputFirstName20"></label>
+                                <input type="int" class="form-control" placeholder="Numéro" disabled>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 other border border-dark">
+                        <h4>LIVRAISON</h4>
                     </div>
                 </div>
             </div>
         </form>
-
-        <form>
-            <div class="container mt-5">
-                <div class="row col-lg-30">
-                    <div class="col-sm-8">
-                        <h3 class="text-center"><small>LIVRAISON</small></h3>
-                    </div>
-                </div>
-            </div>
-        </form>
-
-    </div>
+            <!-- FIN FORM MES ADRESSES-->
 @stop
