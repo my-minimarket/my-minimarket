2190c0c419f426d634d2237ce03cec64289ce94f
cb74 <42967193+cb74@users.noreply.github.com>
2019-02-28 14:22:05 +0100

    Create catalog webpage

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,6 +1,41 @@
 @extends('layouts.page')
 
-@section('title')
-    Liste des produits
+@section('content')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/catalog.css') }}">
+
+    <!--flex row tous les produits -->
+    <div class="row flex-column flex-lg-row">
+    @foreach($products as $product)
+        <!-- product wrapper 01-->
+            <section class="col-12 col-lg-6 mb-3 ">
+                <!-- product_description_wrapper-->
+                <div class="p-3 product-description rounded shadow">
+                    <!-- titre -->
+                    <h3 class="text-uppercase"><small>{{ $product->name }}</small></h3>
+                    <!-- price_wrapper -->
+                    <div class="d-flex ">
+                        <!-- price -->
+                        <p class="h3 mr-auto">
+                            <small>{{ $product->price }} €</small>
+                        </p>
+                    </div>
+                </div> <!-- End product_description_wrapper-->
+                <!-- Product_inner -->
+                <div class="d-flex bg-white rounded shadow flex-column flex-md-row">
+                    <!-- img_product_wrapper-->
+                    <div class="d-flex align-items-end flex-column  p-3">
+                        <!-- img_product-->
+
+                        <!-- img_product_more-->
+                        <a href="#" title="lien vers la page html">
+                            <img class="mb-auto" src="{{ $product->picture }}" alt="product picture"
+                                 title="product picture" style="width: 100%"/></a>
+                    </div>
+                </div>
+            </section><!-- fin du wrapper -->
+        @endforeach
+    </div><!--  div row -->
 @stop
+
+
 
