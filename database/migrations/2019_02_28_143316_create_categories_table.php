f3fcf2ded4262a9ef203688d0aed1b29b16b212f
cb74 <42967193+cb74@users.noreply.github.com>
2019-03-01 11:28:46 +0100

    Create Model and Migration for table categories

---
+++ database/migrations/2019_02_28_143316_create_categories_table.php
@@ -0,0 +1,77 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+            $table->increments('id_category');
+            $table->string('description', 50);
+            $table->timestamps();
+        });
+        DB::table('categories')->insert([
+            'description' => 'Sucré',
+            'created_at' => Carbon::today(),
+            'updated_at' => Carbon::today()
+        ]);
+        DB::table('categories')->insert([
+            'description' => 'Salé',
+            'created_at' => Carbon::today(),
+            'updated_at' => Carbon::today()
+        ]);
+        DB::table('categories')->insert([
+            'description' => 'Boisson',
+            'created_at' => Carbon::today(),
+            'updated_at' => Carbon::today()
+        ]);
+        DB::table('categories')->insert([
+            'description' => 'Divers',
+            'created_at' => Carbon::today(),
+            'updated_at' => Carbon::today()
+        ]);
+        DB::table('categories')->insert([
+            'description' => 'France',
+            'created_at' => Carbon::today(),
+            'updated_at' => Carbon::today()
+        ]);
+        DB::table('categories')->insert([
+            'description' => 'Italie',
+            'created_at' => Carbon::today(),
+            'updated_at' => Carbon::today()
+        ]);
+        DB::table('categories')->insert([
+            'description' => 'Japon',
+            'created_at' => Carbon::today(),
+            'updated_at' => Carbon::today()
+        ]);
+        DB::table('categories')->insert([
+            'description' => 'Etats-Unis',
+            'created_at' => Carbon::today(),
+            'updated_at' => Carbon::today()
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
