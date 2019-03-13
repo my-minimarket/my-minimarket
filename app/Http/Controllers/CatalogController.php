ef974115746185677d8c632fb945a4f120e4879f
cb74 <42967193+cb74@users.noreply.github.com>
2019-03-13 16:31:17 +0100

    Use ORM to display catalog

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,22 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\Category;
+use App\Product;
 
 class CatalogController extends Controller {
 
     public function show($id = null) {
         if ($id === null) {
-            $products = DB::select('SELECT products.id_product, products.name, products.price, products.picture 
-            FROM products 
-            INNER JOIN categories_products
-	        ON categories_products.PRODUCTS_id_product = products.id_product');
+            $products = Product::all();
         } else {
-            $products = DB::select('SELECT products.id_product, products.name, products.price, products.picture 
-            FROM products 
-            INNER JOIN categories_products
-	        ON categories_products.PRODUCTS_id_product = products.id_product
-            WHERE categories_products.CATEGORIES_id_category = ?;', [$id]);
+            $products = Category::find($id)->products()->get();
         }
         return view('catalog', ['headTitle' => 'Catalogue', 'products' => $products]);
     }
