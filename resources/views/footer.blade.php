1984ad9c95fd200aac0c7a9c006798fd866898b1
cb74 <42967193+cb74@users.noreply.github.com>
2019-03-14 16:25:07 +0100

    Footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,36 @@
-<footer>
-    <p>Le footer</p>
+<link rel="stylesheet" type="text/css" href="{{ asset('css/footer.css') }}">
+
+<footer id="footer" class="mt-4 pt-4 px-4">
+
+    <div class="container d-flex flex-row justify-content-around">
+        <div class="col-md-4 mb-3">
+            <h3>A PROPOS DE NOUS</h3>
+            <ul>
+                <li>Qui sommes nous ?</li>
+                <li>Conditions générales de ventes (CGV)</li>
+                <li>Moyens de paiement acceptés</li>
+            </ul>
+        </div>
+
+        <div class="col-md-4 mb-3">
+            <h3>VOUS</h3>
+            <ul>
+                <li>Créer un compte</li>
+                <li>Se connecter</li>
+                <li>Newsletter</li>
+            </ul>
+
+        </div>
+        <div class="col-md-4 mb-3">
+            <h3>NOUS CONTACTER</h3>
+            <address>
+                <ul>
+                    <li>55 avenue de la République</li>
+                    <li>74 960 CRAN-GEVRIER</li>
+                    <li>+33 4 12 34 56 78</li>
+                    <li>contact@myminimarket.com</li>
+                </ul>
+            </address>
+        </div>
+    </div>
 </footer>
