3b174352ad9f48297efc5629288d7e32f190a97f
cb74 <42967193+cb74@users.noreply.github.com>
2019-03-14 11:07:37 +0100

    Getting started registration form

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.page')
 
+@section('headTitle')
+    {{ $headTitle }}
+@endsection
+
 @section('content')
     <H1>COMMANDES</H1>
-
 
     @foreach ($order as $item)
         {{$item->lastname}} {{$item->firstname}} {{$item->date_order}}
     @endforeach
-
 @stop
