6e00544e44cecc8d763a1bd22e0346c8784db8fa
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-03-14 14:45:31 +0100

    gate en cours doux jesus

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Customer;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -25,6 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('update-category', function(Customer $customer)
+        {
+            return $user->isAdmin();
+        });
     }
 }
